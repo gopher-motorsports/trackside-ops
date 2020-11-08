e6fd5e950613345aca3c9d2648f0242709643b14
EC2 Default User <ec2-user@ip-172-31-94-16.ec2.internal>
2020-11-08 22:32:54 +0000

    Fixed settings.py STATIC_ROOT

--- trackside_ops/settings.py
+++ trackside_ops/settings.py
@@ -130,4 +130,6 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static")
 ]
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/tracksideops/static/'
