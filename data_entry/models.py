555774af87757b2e9398295b2787bb31d918f72a
Adrian Leal <adrianlealalonse@gmail.com>
2020-11-08 21:45:58 -0600

    Added Test table and Postgres DB

--- data_entry/models.py
+++ data_entry/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.timezone import now
 
-# Create your models here.
+class Test(models.Model):
+    weather = models.CharField(max_length=100)
+    temperature = models.IntegerField()
+    driver = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    track = models.CharField(max_length=100, blank=True)
+    fast_lap = models.CharField(max_length=100, blank=True)
+    engine = models.CharField(max_length=100, blank=True)
+    software = models.FloatField(blank=True)
+    comments = models.TextField(blank=True)
+    created_at = models.DateTimeField(default=now)
+
