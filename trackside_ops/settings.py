8eb8e9355d9f00006ea68d2cc0e9673518f147f2
Adrian Leal <adrianlealalonse@gmail.com>
2020-11-13 21:45:27 -0600

    back to default db

--- trackside_ops/settings.py
+++ trackside_ops/settings.py
@@ -79,13 +79,8 @@ WSGI_APPLICATION = 'trackside_ops.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Trackside_DB',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres2021',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
 }
 
 
