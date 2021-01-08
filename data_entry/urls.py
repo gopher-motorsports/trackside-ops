434c77923c97f321a4812bc01a587a4d22886b5c
Adrian Leal <adrianlealalonse@gmail.com>
2021-01-07 19:29:42 -0600

    backend for data log and user creation

--- data_entry/urls.py
+++ data_entry/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('data_collection', views.collectData, name='data_collection'),
+    path('collectdata', views.collectData, name='collectdata'),
     path('view_data', views.viewData, name='view_data'),
+    path('create_user', views.createUser, name='create_user'),
 ]
--- trackside_ops/settings.py
+++ trackside_ops/settings.py
@@ -77,10 +77,22 @@ WSGI_APPLICATION = 'trackside_ops.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Production local database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Trackside_DB',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres2021',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
