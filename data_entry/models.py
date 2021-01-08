4aede200adfa64ec154f8a57c9107fb86eb69f24
Adrian Leal <adrianlealalonse@gmail.com>
2021-01-07 19:29:42 -0600

    backend for data log and user creation

--- data_entry/models.py
+++ data_entry/models.py
@@ -9,8 +9,15 @@ class Test(models.Model):
     location = models.CharField(max_length=100)
     track = models.CharField(max_length=100, blank=True)
     fast_lap = models.CharField(max_length=100, blank=True)
+    tires = models.CharField(max_length=100)
+    tire_condition = models.CharField(max_length=100)
     engine = models.CharField(max_length=100, blank=True)
     software = models.FloatField(blank=True)
     comments = models.TextField(blank=True)
     created_at = models.DateTimeField(default=now)
 
+
+class Person(models.Model):
+    email = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    subteam = models.CharField(max_length=100)
--- data_entry/views.py
+++ data_entry/views.py
@@ -6,9 +6,89 @@ def home(request):
     return render(request, 'data_entry/home.html', context)
 
 def collectData(request):
-    context = {}
-    return render(request, 'data_entry/collectdata.html', context)
+    if request.method == 'POST':
+        weather = request.POST['weather']
+        temperature = request.POST['temperature']
+        driver = request.POST['driver']
+        location = request.POST['location']
+        track = request.POST['track']
+        fast_lap = request.POST['fast_lap']
+        tires = request.POST['tires']
+        tire_condition = request.POST['tire_condition']
+        engine = request.POST['engine']
+        software = request.POST['software']
+        comments = request.POST['comments']
+
+        context = {
+            'weather': weather,
+            'temperature': temperature,
+            'driver': driver,
+            'location': location,
+            'track': track,
+            'fast_lap': fast_lap,
+            'tires': tires,
+            'tire_condition': tire_condition,
+            'engine': engine,
+            'software': software,
+            'comments': comments,
+        }
+
+        Test.objects.create(
+            weather = weather,
+            temperature = temperature,
+            driver = driver,
+            location = location,
+            track = track,
+            fast_lap = fast_lap,
+            tires = tires,
+            tire_condition = tire_condition,
+            engine = engine,
+            software = software,
+            comments = comments
+        )
+
+        message = 'Data has been recorded'
+
+        return render(request, 'data_entry/collectdata.html', message)
+
+    else:
+        context = {}
+        return render(request, 'data_entry/collectdata.html', context)
 
 def viewData(request):
     context = {}
     return render(request, 'data_entry/viewdata.html', context)
+
+def createUser(request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subteam = request.POST['subteam']
+
+        context = {
+            'name': name,
+            'email': email,
+            'subteam': subteam,
+        }
+
+        if Person.objects.filter(email=email).exists():
+            message = "User already exist"
+            context = {
+                'message'
+            }
+            return render(request, 'data_entry/createuser.html', context)
+
+        else:
+            Person.objects.create(
+                name = name,
+                email = email,
+                subteam = subteam,
+            )
+
+            message = 'User has been created'
+
+            return render(request, 'data_entry/createuser.html', context)
+
+    else:
+        context = {}
+        return render(request, 'data_entry/createuser.html', context)
