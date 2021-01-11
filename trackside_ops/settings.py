920ab14145fa162f9b3111609c9b5e0abb3ee751
EC2 Default User <ec2-user@ip-172-31-94-16.ec2.internal>
2021-01-11 03:28:52 +0000

    Updates settings to use env vars for DB auth

--- trackside_ops/settings.py
+++ trackside_ops/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
-from .keys import keys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,21 +77,14 @@ WSGI_APPLICATION = 'trackside_ops.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# Production local database
+# Production database
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Trackside_DB',
-        'USER': 'gopher',
-        'PASSWORD': keys['password'],
-        'HOST': keys['host'],
+        'NAME': os.environ.get("POSTGRES_DB"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("POSTGRES_HOST"),
         'PORT': '5432',
     }
 }
