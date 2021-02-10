12d59c41bfa6738a84b27fd82e03433a1be89754
Adrian Leal <adrianlealalonse@gmail.com>
2021-02-10 16:43:23 -0600

    API running for tests. Need to add persons and some testing

--- data_entry/urls.py
+++ data_entry/urls.py
@@ -1,9 +1,15 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+router.register(r'tests', views.TestViewSet)
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('collectdata', views.collectData, name='collectdata'),
     path('view_data', views.viewData, name='view_data'),
     path('create_user', views.createUser, name='create_user'),
+    path('api/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
