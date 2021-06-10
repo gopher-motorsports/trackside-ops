f2dae7352c2378cc572c9dc71103f82456d53854
Johnny Rajan <johnny.rajan@gmail.com>
2021-06-10 18:14:50 -0500

    added port back

--- trackside_ops/settings.py
+++ trackside_ops/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'USER': os.environ.get("POSTGRES_USER"),
         'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
         'HOST': os.environ.get("POSTGRES_HOST"),
-        'PORT': '',
+        'PORT': '5432',
     }
 }
 
